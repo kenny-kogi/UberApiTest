5054d35c5c0bfec67bbb6e2a4b30dcb169cb522f
kenny-kogi <mwangiken497@gmail.com>
2022-04-05 17:11:24 +0300

    bugfix: add car/reference/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cars, only: [:index]
+  resources :cars, only: [:index, :create]
   resources :location_reports, only: [:create]
   get "cars/:id/reports", to: "cars#reports"
 end
